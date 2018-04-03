c3c0133c31774f26e48a11960297cc10f38d0899
Mark Mims <mark.mims@ubuntu.com>
2018-04-03 15:23:52 -0600

    adding hive thriftserver to spark-minimal

--- spark-minimal/docker-entrypoint.sh
+++ spark-minimal/docker-entrypoint.sh
@@ -8,8 +8,13 @@ if [ "${1:0:1}" = '-' ]; then
 fi
 
 if [ ! -z "$HADOOP_NAMENODE" ]; then
-  cp $SPARK_TEMPLATE_PATH/*-site.xml $SPARK_CONF_PATH/
+  cp $SPARK_TEMPLATE_PATH/core-site.xml $SPARK_CONF_PATH/
+  cp $SPARK_TEMPLATE_PATH/hdfs-site.xml $SPARK_CONF_PATH/
   sed -i "s/myhdfs/${HADOOP_NAMENODE}/" $SPARK_CONF_PATH/core-site.xml
+fi
+if [ ! -z "$HIVE_THRIFTSERVER" ]; then
+  cp $SPARK_TEMPLATE_PATH/hive-site.xml $SPARK_CONF_PATH/
+  sed -i "s/myhdfs/${HIVE_THRIFTSERVER}/" $SPARK_CONF_PATH/hive-site.xml
 fi
 
 echo
